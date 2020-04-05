e8f6c945ed8dc704daf77efdd429a7b1ffda7da2
DANNIEL <donghoon149@gmail.com>
2020-04-05 20:30:46 +0900

    first commit

---
+++ client/src/services/productService.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+export default {
+  getAll: async () => {
+    let res = await axios.get(`/api/product`);
+    return res.data || [];
+  },
+};
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// IMPORT MODELS
+require("./models/Product");
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+
+corsOptions = {
+  origin: "http://deathnote-gg.herokuapp.com/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+//IMPORT ROUTES
+require("./routes/productRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
