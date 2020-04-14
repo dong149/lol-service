17156f4579e35ff4240e16ff434242782e439776
DANNIEL <donghoon149@gmail.com>
2020-04-14 22:18:07 +0900

    fix:heroku

--- index.js
+++ index.js
@@ -58,15 +58,6 @@ app.use(bodyParser.json());
 //IMPORT ROUTES
 require("./routes/productRoutes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 // LOL api
 app.get("/", (req, res) => res.send(`TOKEN: ${TOKEN}`));
 
@@ -97,9 +88,16 @@ const getLeagueByEncryptedId = async (res, encryptedId) => {
     })
     .catch(console.log);
 };
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
-console.log(PORT);
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
