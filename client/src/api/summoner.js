62def911ec3f55f82466eb2a085c4003b5ec034d
DANNIEL <donghoon149@gmail.com>
2020-04-15 16:02:14 +0900

    fix:heroku

--- client/src/api/summoner.js
+++ client/src/api/summoner.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
 let BASE_URL;
-if (process.env.NODE_ENV == "production") {
-  BASE_URL = "https://league-of-legend-service.herokuapp.com";
-} else {
-  BASE_URL = "http://localhost:5000";
-}
+// if (process.env.NODE_ENV == "production") {
+//   BASE_URL = "https://league-of-legend-service.herokuapp.com";
+// } else {
+//   BASE_URL = "http://localhost:5000";
+// }
 
 const baseAPI = axios.create({
   baseURL: BASE_URL,
@@ -13,13 +14,11 @@ const baseAPI = axios.create({
 
 export const api = {
   getSummonerByName: async (summonerName) => {
-    console.log(summonerName);
-    const res = await baseAPI.get(`/api/summoner-by-name?name=${summonerName}`);
+    const res = await axios.get(`/api/summoner-by-name?name=${summonerName}`);
     return res.data;
   },
   getLeagueByEncryptedId: async (encryptedId) => {
-    console.log(encryptedId);
-    const result = await baseAPI.get(
+    const result = await axios.get(
       `/api/league-by-encrypted-id?id=${encryptedId}`
     );
     return result.data;
