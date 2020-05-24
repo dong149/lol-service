8591d9d19c566fd7a61acb2d97a1edb23c001f19
DANNIEL <donghoon149@gmail.com>
2020-05-24 18:55:55 +0900

    fix base_url

--- client/src/services/reviewService.js
+++ client/src/services/reviewService.js
@@ -1,22 +1,33 @@
 import axios from "axios";
+
+let BASE_URL;
+if (process.env.NODE_ENV === "production") {
+  BASE_URL = "https://donghoon.tk";
+} else {
+  BASE_URL = "http://localhost:3000";
+}
+const baseAPI = axios.create({
+  baseURL: BASE_URL,
+});
+
 export default {
   //   getReviewAll: async () => {
   //     let res = await axios.get(`/api/review`);
   //     return res.data || [];
   //   },
   getReview: async () => {
-    let res = await axios.get(`/api/review`);
+    let res = await baseAPI.get(`/api/review`);
     // console.log(res.data);
     return res.data || [];
   },
   getSummonerReview: async (name) => {
-    let res = await axios.get(`api/review/name/${name}`);
+    let res = await baseAPI.get(`api/review/name/${name}`);
     // console.log(res.data);
     return res.data || [];
   },
 
   postReview: async (object) => {
-    await axios
+    await baseAPI
       .post(`/api/review`, object)
       .then((res) => {
         console.log("post");
